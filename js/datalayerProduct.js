819f5f78e0a501b0e0349101ecca28a947942646
saiganesh072 <saiado3041@gmail.com>
2024-05-01 18:13:31 +0530

    product keywords

--- js/datalayerProduct.js
+++ js/datalayerProduct.js
@@ -9,11 +9,122 @@ var userName = getCookieValue("CustomerName");
 var pageName = window.location.pathname.split("/").pop().split(".")[0];
 var pageUrl = window.location.href;
 
-var productId = document
-  .querySelector(".js-addcart-detail")
-  .getAttribute("data-product-id");
+var productId = document.querySelector(".js-addcart-detail").getAttribute("data-product-id");
 var productName = document.querySelector(".js-name-detail").innerText;
 var productPrice = document.querySelector(".mtext-106").innerText;
+
+var productKeyMap = {
+  "Esprit Ruffle Shirt": [
+    "white",
+    "t-shirt",
+    "casual wear",
+    "short sleeves",
+    "black",
+    "jeans",
+    "silver",
+    "belt",
+    "women",
+  ],
+  "Herschel supply": [
+    "white",
+    "shirt",
+    "long sleeves",
+    "button-up",
+    "collared",
+    "pink",
+    "trousers",
+    "formal wear",
+    "women",
+  ],
+  "Only Check Trouser": ["blue", "checkered", "shirt", "short sleeves", "denim", "jeans", "men"],
+  "Classic Trench Coat": [
+    "olive",
+    "green",
+    "coat",
+    "fur-lined collar",
+    "belted",
+    "black",
+    "pants",
+    "women",
+    "long sleeves",
+  ],
+  "Front Pocket Jumper": [
+    "denim",
+    "shirt",
+    "jeans",
+    "black",
+    "skirt",
+    "short sleeves",
+    "button-up",
+    "collared",
+  ],
+  "Vintage Inspired Classic": [
+    "black",
+    "watch",
+    "analog",
+    "leather strap",
+    "round dial",
+    "minimalist design",
+    "rose gold",
+  ],
+  "Shirt in Stretch Cotton": [
+    "white",
+    "grey",
+    "shirt",
+    "blazer",
+    "blue",
+    "jeans",
+    "black",
+    "belt",
+    "stylish",
+    "checkered",
+  ],
+  "Pieces Metallic Printed": [
+    "white",
+    "t-shirt",
+    "casual wear",
+    "short sleeves",
+    "cactus",
+    "print",
+    "green",
+    "denim",
+    "jeans",
+    "blue",
+    "silver",
+    "belt",
+    "buckle",
+  ],
+  "T-Shirt with Sleeve": [
+    "white",
+    "t-shirt",
+    "casual wear",
+    "short sleeves",
+    "blue",
+    "jeans",
+    "silver",
+  ],
+  "Pretty Little Thing": ["black", "top", "red", "rose", "women", "green"],
+  "Mini Silver Mesh Watch": [
+    "black",
+    "silver",
+    "watch",
+    "analog",
+    "chain",
+    "strap",
+    "round dial",
+    "minimalist design",
+  ],
+  "Square Neck Back": [
+    "grey",
+    "black",
+    "t-shirt",
+    "casual wear",
+    "short sleeves",
+    "trousers",
+    "women",
+  ],
+};
+var productKeys = productKeyMap[productName] || [];
 
 var digitalData = {
   account: {
@@ -31,6 +142,7 @@ var digitalData = {
     id: productId,
     name: productName,
     price: productPrice,
+    Keywords: productKeys,
     // thumbnailUrl: productThumbnail,
     // categoryId: productCategoryId,
     // productType: productType,
